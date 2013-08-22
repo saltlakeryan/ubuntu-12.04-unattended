e8ab849d558fa3464fd7f028aa8a6cc5274648f3
Ryan Taylor <ryant@nanofab.utah.edu>
2013-08-22 09:22:57 -0600

    Refactor

--- make.sh
+++ make.sh
@@ -1,22 +1,44 @@
 #!/bin/bash
-BUILD=/tmp/custom-cd/
-IMAGE=/tmp/custom.iso
 
-#mount original image
-mkdir -p /tmp/custom-cd-source
-mount -o loop $1 /tmp/custom-cd-source
+function initialize {
+  BUILD=/tmp/custom-cd/
+  IMAGE=/vagrant/custom.iso
+  MOUNTIMAGE=/tmp/custom-cd-source
+  HERE=`scriptdir`
+  STARTINGDIR=`pwd`
+  ORIGINALIMAGE=$1
+}
 
-#copy files to writable place
-rsync -rv /tmp/custom-cd-source/ $BUILD
-chmod -R 777 $BUILD
+function main {
+  initialize $1
+  #mount original image
+  mkdir -p $MOUNTIMAGE
+  mount -o loop $1 $MOUNTIMAGE
 
-#copy files from this project
-rsync -rv ./cd-files/ $BUILD
+  #copy files to writable place
+  rsync -rv $MOUNTIMAGE/ $BUILD
+  chmod -R 777 $BUILD
 
-#make iso
-mkisofs -r -V "Custom Ubuntu Install CD" \
-            -cache-inodes \
-            -J -l -b isolinux/isolinux.bin \
-            -c isolinux/boot.cat -no-emul-boot \
-            -boot-load-size 4 -boot-info-table \
-            -o $IMAGE $BUILD
+  #copy files from this project
+  rsync -rv $HERE/cd-files/ $BUILD
+
+  #make iso
+  cd $BUILD
+  mkisofs -r -V "Custom Ubuntu Install CD" \
+              -cache-inodes \
+              -J -l -b isolinux/isolinux.bin \
+              -c isolinux/boot.cat -no-emul-boot \
+              -boot-load-size 4 -boot-info-table \
+              -o $IMAGE $BUILD
+  cd -
+}
+
+function scriptdir {
+  pushd `dirname $0` > /dev/null
+  SCRIPTPATH=`pwd -P`
+  popd > /dev/null
+  echo $SCRIPTPATH
+}
+
+main $1
+
